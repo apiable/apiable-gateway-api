486b1795984f090d2c789dc8501dd79e39d7e753
Peter Porttinen <peter.porttinen@apiable.io>
2025-08-21 16:38:09 +0300

    wip manage api keys

--- src/main/kotlin/io/apiable/gateways/adapter/Gateway.kt
+++ src/main/kotlin/io/apiable/gateways/adapter/Gateway.kt
@@ -148,6 +148,8 @@ interface ApiGateway {
     fun validateExistsOnGateway(conf: Conf, plan: Plan): Map<String,String>
 
     fun searchApiKeys(conf: Conf, nameQuery: String, pageSize: Int, position: String?): APIKeySearch
+    fun setAPIKeyStatus(conf: Conf, apiKeyIntegrationId: String, enabled: Boolean): Boolean
+    fun deleteAPIKey(conf: Conf, apiKeyIntegrationId: String): Boolean
 
 }
 
